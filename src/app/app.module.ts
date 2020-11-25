c2ec3774b9397c2899188c008a95b5a92d56e17d
sealzhk <sealzhk@users.noreply.github.com>
2020-11-25 13:09:15 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModelDrivenFormsComponent } from './model-driven-forms/model-driven-forms.component';
+import { TemplateDrivenFormsComponent } from './template-driven-forms/template-driven-forms.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ToggleComponent} from './template-driven-forms/toggle/toggle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelDrivenFormsComponent,
+    TemplateDrivenFormsComponent,
+    ToggleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/template-driven-forms/toggle/toggle.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-toggle',
+  templateUrl: './toggle.component.html'
+})
+export class ToggleComponent implements OnInit {
+  constructor() { }
+  form3a;
+  form31a;
+  ngOnInit(): void {
+  }
+
+}
